a837a090f030e9c0336cfbd9a817fdd174b739f9
Nipun Mendes <nipunmendes@gmail.com>
2024-03-17 17:58:55 +0530

    Initial commit

--- back-end/src/main/java/com/example/backend/controller/AvailabilityController.java
+++ back-end/src/main/java/com/example/backend/controller/AvailabilityController.java
@@ -1,13 +1,16 @@
 package com.example.backend.controller;
 
 import com.example.backend.exception.UserNotFoundException;
+import com.example.backend.model.Appointment;
 import com.example.backend.model.Availability;
 import com.example.backend.model.Patient;
 import com.example.backend.model.Technician;
 import com.example.backend.repository.AvailabilityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -40,5 +43,21 @@ public class AvailabilityController {
                 .orElseThrow(() -> new UserNotFoundException(availabilityId));
         availabilityRepo.delete(availability);
         return availability;
+    }
+
+    @PutMapping("/Availability/{availabilityId}")
+    public Availability updateAvailability(
+            @PathVariable Integer availabilityId,
+            @RequestBody Availability updatedAvailability
+    ) {
+        return availabilityRepo.findById(availabilityId)
+                .map(availability -> {
+                    availability.setDate(updatedAvailability.getDate());
+                    availability.setTime(updatedAvailability.getTime());
+                    availability.setAvailability(updatedAvailability.getAvailability());
+                    return availabilityRepo.save(availability);
+                })
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
+                        "Availability with ID " + availabilityId + " not found"));
     }
 }
